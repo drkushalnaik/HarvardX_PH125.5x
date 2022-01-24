39209dd9ee3041bff708ba73e90ce9abc9c30017
Kushal Naik <kushal.naik@gmail.com>
2022-01-24 00:15:42 -0800

    Code from Section 2: Basic Unix

--- testcode.R
+++ testcode.R
@@ -1,1 +1,35 @@
-#test add info
+#creating a test file  to test git, github, RStudio integration
+
+# Learning UNIX
+
+# Sending the following to the terminal using ctrl alt enter
+
+echo "hello world"
+
+# find the working directory
+pwd
+# /c/Users/Kushal/Documents/Git/HarvardX_PH125.5x
+
+# Exploring UNIX commands
+ls
+mkdir subprojects
+mkdir docs teaching
+ls
+mkdir junk
+ls
+rmdir junk
+ls
+cd subprojects
+# /c/Users/Kushal/Documents/Git/HarvardX_PH125.5x/subprojects
+pwd
+cd ..
+cd .
+cd ~
+cd ~/subprojects
+cd subprojects
+cd ../..
+cd
+cd -
+cd cd /c/Users/Kushal/Documents/Git/HarvardX_PH125.5x
+cd C:\Users\Kushal\Documents\Git\HarvardX_PH125.5x
+pwd
